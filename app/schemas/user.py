7876ee3198b8d92faf4a1b30084b33acbc558f18
stefano.greco@polis-net.it <stefano.greco@polis-net.it>
2025-06-03 17:14:43 +0200

    aggiunto schema per user

---
+++ app/schemas/user.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    nome: Optional[str] = None
+
+
+class UserRead(BaseModel):
+    id: UUID
+    email: EmailStr
+    nome: Optional[str]
+
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+
+class TokenData(BaseModel):
+    user_id: UUID
